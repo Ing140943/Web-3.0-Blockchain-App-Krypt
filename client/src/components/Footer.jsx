087d866aec42a825a6dde01e944394687f04f1a5
Ing140943 <skladeeing@gmail.com>
2022-04-14 15:52:38 +0700

    Fix stupid bug

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,8 +3,8 @@ import logo from '../../images/logo.png';
 const Footer = () => {
     return (
         <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
-            <div classNmae="w-full flex sm:flex-row flex-col justify-between items-center my-4">
-                <div classNmae="flex flex-[0.5] justify-center items-center">
+            <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
+                <div className="flex flex-[0.5] justify-center items-center">
                     <img src={logo} alt="logo" className="w-32"/>
                 </div>
                 <div className="flex flex-1 justify-evently items-center flex-wrap sm:mt-0 mt-5 w-full">
